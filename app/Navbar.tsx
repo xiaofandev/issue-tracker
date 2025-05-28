4f3170d0a223a4a23b5a29e55e4a50cb2b8d38ff
xiaofandev <xiaofandev@126.com>
2025-05-29 01:51:50 +0800

    style the active link in the navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,4 +1,7 @@
+"use client";
+import classNames from "classnames";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { FaBug } from "react-icons/fa";
 
 const Navbar = () => {
@@ -12,6 +15,8 @@ const Navbar = () => {
       href: "/issues",
     },
   ];
+  const currentPath = usePathname();
+  console.log(currentPath);
   return (
     <nav className="flex space-x-8 border-b mb-4 px-4 h-12 items-center">
       <Link href="/">
@@ -22,7 +27,11 @@ const Navbar = () => {
           <li key={index}>
             <Link
               href={link.href}
-              className="text-zinc-500 hover:text-zinc-800"
+              className={classNames({
+                "text-zinc-500": currentPath != link.href,
+                "hover:hover:text-zinc-800": true,
+                "text-zinc-900": currentPath == link.href,
+              })}
             >
               {link.label}
             </Link>
