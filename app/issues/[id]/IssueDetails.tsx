8a6003e071b1238bd0696315ba77ef184cc64f3e
肖帆 <xiaofandev@126.com>
2025-06-13 17:19:10 +0800

    clear the redundant div

--- app/issues/[id]/IssueDetails.tsx
+++ app/issues/[id]/IssueDetails.tsx
@@ -1,21 +1,17 @@
 import StatusBadge from "@/app/component/StatusBadge";
 import { Issue } from "@prisma/client";
-import { Box, Heading, Card, Text } from "@radix-ui/themes";
+import { Box, Heading, Card, Text, Flex } from "@radix-ui/themes";
 import React from "react";
 
 const IssueDetails = ({ issue }: { issue: Issue }) => {
   return (
     <>
-      <div className="mb-2">
-        <Heading>{issue.title}</Heading>
-      </div>
-      <div className="flex space-x-4 items-center mb-2">
+      <Heading mb="2">{issue.title}</Heading>
+      <Flex className="space-x-4 items-center" mb="2">
         <StatusBadge status={issue.status} />
         <Text>{issue.createdAt.toDateString()}</Text>
-      </div>
-      <div>
-        <Card>{issue.description}</Card>
-      </div>
+      </Flex>
+      <Card>{issue.description}</Card>
     </>
   );
 };
