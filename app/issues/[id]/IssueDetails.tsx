5400024f76f8a8d006385e723797401657250036
肖帆 <xiaofandev@126.com>
2025-06-13 17:00:21 +0800

    Update the details page layout

--- app/issues/[id]/IssueDetails.tsx
+++ app/issues/[id]/IssueDetails.tsx
@@ -6,10 +6,10 @@ import React from "react";
 const IssueDetails = ({ issue }: { issue: Issue }) => {
   return (
     <>
-      <div>
+      <div className="mb-2">
         <Heading>{issue.title}</Heading>
       </div>
-      <div className="flex space-x-4 items-center">
+      <div className="flex space-x-4 items-center mb-2">
         <StatusBadge status={issue.status} />
         <Text>{issue.createdAt.toDateString()}</Text>
       </div>
