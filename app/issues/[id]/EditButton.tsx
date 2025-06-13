0dd7f63726d5dbfac7cc9c4eac6bf5176cc15ce9
肖帆 <xiaofandev@126.com>
2025-06-13 17:10:57 +0800

    Add Icon on EditButton

--- app/issues/[id]/EditButton.tsx
+++ app/issues/[id]/EditButton.tsx
@@ -1,10 +1,11 @@
-import { Box, Button } from "@radix-ui/themes";
+import { Button } from "@radix-ui/themes";
 import Link from "next/link";
-import React from "react";
+import { Pencil2Icon } from "@radix-ui/react-icons";
 
 const EditButton = ({ issueId }: { issueId: number }) => {
   return (
     <Button>
+      <Pencil2Icon />
       <Link href={`/issues/${issueId}/edit`}>Edit Issue</Link>
     </Button>
   );
