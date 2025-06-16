bac6b3a3b96669cd35a762c64de88fe70690db8f
肖帆 <xiaofandev@126.com>
2025-06-16 15:19:49 +0800

    Build an api for updating issues

---
+++ app/api/issues/[id]/route.ts
@@ -0,0 +1,19 @@
+import { IssueSchema } from "@/app/validation/schema";
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(request: NextRequest) {
+  const issue = await request.json();
+  const validation = IssueSchema.safeParse(issue);
+
+  if (!validation.success) {
+    return NextResponse.json({ error: validation.error }, { status: 400 });
+  }
+
+  const updatedIssue = await prisma.issue.update({
+    where: { id: 1 },
+    data: { title: issue.title, description: issue.description },
+  });
+
+  return NextResponse.json(updatedIssue);
+}
