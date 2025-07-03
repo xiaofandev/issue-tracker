488569d7534d9cba9394a14d6f84479608c35c72
肖帆 <xiaofandev@126.com>
2025-07-03 19:04:08 +0800

    Update the patch api

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,24 +1,24 @@
-import { IssueSchema } from "@/app/validation/schema";
+import { PatchIssueSchema } from "@/app/validation/schema";
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function PATCH(
-  request: NextRequest,
-  { params }: { params: { id: string } }
-) {
+export async function PATCH(request: NextRequest) {
   const issue = await request.json();
-  const validation = IssueSchema.safeParse(issue);
 
+  // Check if the form data is valid
+  const validation = PatchIssueSchema.safeParse(issue);
   if (!validation.success) {
     return NextResponse.json({ error: validation.error }, { status: 400 });
   }
 
+  // Check if the issue exists
   const issueFromDatabase = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
+    where: { id: issue.id },
   });
   if (!issueFromDatabase)
     return NextResponse.json({ error: "Issue not found" }, { status: 404 });
 
+  // Check if the assignee user exists
   if (issue.assignToUser) {
     const userFromDatabase = await prisma.user.findUnique({
       where: { id: issue.assignToUser },
@@ -27,8 +27,9 @@ export async function PATCH(
       return NextResponse.json({ error: "User not found" }, { status: 404 });
   }
 
+  // Do update the issue info
   const updatedIssue = await prisma.issue.update({
-    where: { id: parseInt(params.id) },
+    where: { id: issue.id },
     data: {
       title: issue.title,
       description: issue.description,
--- app/validation/schema.ts
+++ app/validation/schema.ts
@@ -1,7 +1,14 @@
 import z from "zod";
 
-export const IssueSchema = z.object({
+export const NewIssueSchema = z.object({
   title: z.string().min(1),
   description: z.string().min(1),
-  assignToUser: z.string().optional(),
+  assignToUser: z.string().optional().nullable(),
+});
+
+export const PatchIssueSchema = z.object({
+  id: z.number(),
+  title: z.string().min(1).optional(),
+  description: z.string().min(1).optional(),
+  assignToUser: z.string().optional().nullable(),
 });
