1bfb9d21ff1a60edbcda698fdaaac0a97ec5cca9
肖帆 <xiaofandev@126.com>
2025-07-12 08:44:19 +0800

    Update layout

--- app/IssueSummary.tsx
+++ app/IssueSummary.tsx
@@ -11,20 +11,43 @@ interface Props {
 }
 
 const IssueSummary = ({ open, closed, inProgress }: Props) => {
-  const statuses: { label: string; value: Status; count: number }[] = [
-    { label: "Open Issues", value: "OPEN", count: open },
-    { label: "Closed Issues", value: "CLOSED", count: closed },
-    { label: "In_Progress Issues", value: "IN_PROGRESS", count: inProgress },
+  const statuses: {
+    label: string;
+    value: Status;
+    count: number;
+    color: string;
+  }[] = [
+    {
+      label: "Open Issues",
+      value: "OPEN",
+      count: open,
+      color: "red-400",
+    },
+    {
+      label: "Closed Issues",
+      value: "CLOSED",
+      count: closed,
+      color: "green-400",
+    },
+    {
+      label: "InProgress Issues",
+      value: "IN_PROGRESS",
+      count: inProgress,
+      color: "orange-400",
+    },
   ];
   return (
     <Flex gap="2">
       {statuses.map((status) => (
-        <Card key={status.value}>
+        <Card key={status.value} className={`bg-${status.color}`}>
           <Flex direction="column" gap="2">
-            <Link className="font-bold" href={`/issues?status=${status.value}`}>
+            <Link
+              className="font-bold text-lg"
+              href={`/issues?status=${status.value}`}
+            >
               {status.label}
             </Link>
-            <Text>{status.count}</Text>
+            <Text className="text-lg font-medium">{status.count}</Text>
           </Flex>
         </Card>
       ))}
