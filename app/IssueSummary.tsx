877e72a3b1aa1bd3adb467b7054af3b26f3c18bd
肖帆 <xiaofandev@126.com>
2025-07-12 09:52:20 +0800

    Fix issue color not work

--- app/IssueSummary.tsx
+++ app/IssueSummary.tsx
@@ -1,4 +1,3 @@
-import prisma from "@/prisma/client";
 import { Status } from "@prisma/client";
 import { Card, Flex, Text } from "@radix-ui/themes";
 import Link from "next/link";
@@ -21,26 +20,30 @@ const IssueSummary = ({ open, closed, inProgress }: Props) => {
       label: "Open Issues",
       value: "OPEN",
       count: open,
-      color: "red-400",
+      color: "bg-red-400",
     },
     {
       label: "Closed Issues",
       value: "CLOSED",
       count: closed,
-      color: "green-400",
+      color: "bg-green-400",
     },
     {
       label: "InProgress Issues",
       value: "IN_PROGRESS",
       count: inProgress,
-      color: "orange-400",
+      color: "bg-orange-400",
     },
   ];
   return (
     <Flex gap="2">
       {statuses.map((status) => (
-        <Link className="text-lg" href={`/issues?status=${status.value}`}>
-          <Card key={status.value} className={`bg-${status.color}`}>
+        <Link
+          className="text-lg"
+          href={`/issues?status=${status.value}`}
+          key={status.value}
+        >
+          <Card className={status.color}>
             <Flex direction="column" gap="2">
               {status.label}
               <Text className="text-xl font-bold">{status.count}</Text>
