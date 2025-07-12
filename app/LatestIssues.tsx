2b410c5eb0bb306c93b747a3d9f0bdb8d52f109c
肖帆 <xiaofandev@126.com>
2025-07-12 08:32:45 +0800

    Add avatar

--- app/LatestIssues.tsx
+++ app/LatestIssues.tsx
@@ -1,4 +1,4 @@
-import { Card, Flex, Heading, Table, Text } from "@radix-ui/themes";
+import { Avatar, Card, Flex, Heading, Table, Text } from "@radix-ui/themes";
 import React from "react";
 import StatusBadge from "./component/StatusBadge";
 import prisma from "@/prisma/client";
@@ -8,6 +8,7 @@ const LatestIssues = async () => {
   const latestIssues = await prisma.issue.findMany({
     orderBy: [{ id: "desc" }],
     take: 5,
+    include: { user: true },
   });
   return (
     <Card>
@@ -17,14 +18,23 @@ const LatestIssues = async () => {
           {latestIssues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.RowHeaderCell>
-                <Flex direction="column" align="start" gap="2">
-                  <Link
-                    className="text-sm font-bold"
-                    href={`/issues/${issue.id}`}
-                  >
-                    {issue.title}
-                  </Link>
-                  <StatusBadge status={issue.status} />
+                <Flex justify="between" align="center">
+                  <Flex direction="column" align="start" gap="2">
+                    <Link
+                      className="text-sm font-bold"
+                      href={`/issues/${issue.id}`}
+                    >
+                      {issue.title}
+                    </Link>
+                    <StatusBadge status={issue.status} />
+                  </Flex>
+                  {issue.user && (
+                    <Avatar
+                      fallback="?"
+                      src={issue.user.image!}
+                      radius="full"
+                    />
+                  )}
                 </Flex>
               </Table.RowHeaderCell>
             </Table.Row>
